4d9681b462c032356c0cfc1e2105e393fd0dbe76
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-13 14:53:52 +0800

    style: 🔥 remove license comment in all files

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# 本文件是 CurseForgeModpackDownloader 的一部分。
-
-# CurseForgeModpackDownloader 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
-
-# 发布 CurseForgeModpackDownloader 是希望它能有用，但是并无保障；甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。
-
-# 你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。
 import os
 import json
 import shutil
