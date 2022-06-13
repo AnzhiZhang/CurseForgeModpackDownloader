b08807a974578a38f673f0010ebc8f13bc32e99b
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-13 15:10:34 +0800

    feat: ✨ add command parameters to ignore license notice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import shutil
 import hashlib
@@ -171,15 +172,16 @@ if os.path.isdir(PATH.TEMP_DIR_PATH):
 os.mkdir(PATH.TEMP_DIR_PATH)
 
 # 版权声明
-accept = askokcancel(
-    '版权声明',
-    'Copyright © 2022 Andy Zhang\n'
-    '本程序是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。\n'
-    '发布该程序是希望它能有用，但是并无保障；甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。\n'
-    '你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 https://www.gnu.org/licenses/。'
-)
-if not accept:
-    exit()
+if '--no-license' not in sys.argv:
+    accept = askokcancel(
+        '版权声明',
+        'Copyright © 2022 Andy Zhang\n'
+        '本程序是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。\n'
+        '发布该程序是希望它能有用，但是并无保障；甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。\n'
+        '你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 https://www.gnu.org/licenses/。'
+    )
+    if not accept:
+        exit()
 
 # 计算路径
 file_path = askopenfilename().replace('/', os.sep)
