3ed3dc24a4c5c26890e7135b9b79500589bb6d64
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-13 12:02:42 +0800

    feat(requester): ✨ add json method for Response

--- utils/requester.py
+++ utils/requester.py
@@ -5,6 +5,7 @@
 # 发布 CurseForgeModpackDownloader 是希望它能有用，但是并无保障；甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。
 
 # 你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。
+import json
 from urllib.parse import quote, urlencode
 from urllib.request import Request, urlopen
 
@@ -28,6 +29,9 @@ class Response:
     @property
     def text(self):
         return self.content.decode(self.__charset)
+
+    def json(self):
+        return json.loads(self.content)
 
 
 class Requester:
