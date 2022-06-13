f8e549c8e42af9263804600d73f08abe4211e0b1
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-13 12:44:40 +0800

    feat(requester): ✨ add search modpack

--- utils/requester.py
+++ utils/requester.py
@@ -63,3 +63,35 @@ class Requester:
             f'https://addons-ecs.forgesvc.net/api/v2/addon/'
             f'{project_id}/file/{file_id}/download-url'
         )
+
+    @classmethod
+    def search_modpack(
+            cls,
+            game_version: str = None,
+            search_filter: str = None,
+            sort: int = None,
+            index: int = 0
+    ):
+        """
+        Search modpacks.
+        :param game_version: Game version string.
+        :param sort: Sorting rule.
+        :param search_filter: Filter to search, a string.
+        :param index: Page index.
+        :return: Response.
+        """
+        params = {
+            'gameId': 432,
+            'sectionId': 4471,
+            'index': index
+        }
+        if game_version:
+            params['gameVersion'] = game_version
+        if search_filter:
+            params['searchFilter'] = search_filter
+        if sort:
+            params['sort'] = sort
+        return cls.get(
+            'https://addons-ecs.forgesvc.net/api/v2/addon/search',
+            params=params
+        )
