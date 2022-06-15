fc1abcbea671f623b68ff1c9f453d602e5489074
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-15 16:39:39 +0800

    refactor(ui): ♻️ remove spacel label in `Search` frame

--- utils/window/frames/search.py
+++ utils/window/frames/search.py
@@ -6,9 +6,7 @@ class Search(Frame):
         super().__init__(master, height=50)
 
         self.search_entry = Entry(self)
-        self.space1_label = Label(self, width=1)
         self.search_button = Button(self, text='搜索', background='white')
 
         self.search_entry.pack(side='left', fill='both', expand=True)
-        self.space1_label.pack(side='left', fill='both')
-        self.search_button.pack(side='left', fill='y')
+        self.search_button.pack(side='left', fill='y', padx=(10, 0))
