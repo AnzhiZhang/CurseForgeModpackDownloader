42e1355d3baccba7f000f76d4f96a726967fce74
Lily <Lily@lily--chou@outlook.com>
2022-06-19 18:27:30 +0800

    TEST-Remove Codes About 'High DPI on Windows'

--- utils/window/main.py
+++ utils/window/main.py
@@ -1,6 +1,6 @@
 import sys
 from threading import Thread
-from ctypes import windll
+# from ctypes import windll
 from tkinter import Tk
 from tkinter.messagebox import askokcancel
 
@@ -22,27 +22,27 @@ class Main(Tk):
         self.title(NAME_WITH_SPACE)
         self.iconbitmap(PATH.ICON_PATH)
 
-        # High DPI
-        # https://stackoverflow.com/questions/62794931/high-dpi-tkinter-re-scaling-when-i-run-it-in-spyder-and-when-i-run-it-direct-in
-        windll.shcore.SetProcessDpiAwareness(2)
-        scale_factor = windll.shcore.GetScaleFactorForDevice(0) / 75
-        self.tk.call('tk', 'scaling', scale_factor)
+    # # High DPI
+    # # https://stackoverflow.com/questions/62794931/high-dpi-tkinter-re-scaling-when-i-run-it-in-spyder-and-when-i-run-it-direct-in
+    #    windll.shcore.SetProcessDpiAwareness(2)
+    #    scale_factor = windll.shcore.GetScaleFactorForDevice(0) / 75
+    #    self.tk.call('tk', 'scaling', scale_factor)
 
-        self.__title_frame = frames.Title(self)
-        self.__search_frame = frames.Search(self)
-        self.__show_frame = frames.Show(self)
-        self.__filters_frame = frames.Filters(self)
-        self.__buttons_frame = frames.Buttons(self)
+    #    self.__title_frame = frames.Title(self)
+    #    self.__search_frame = frames.Search(self)
+    #    self.__show_frame = frames.Show(self)
+    #    self.__filters_frame = frames.Filters(self)
+    #    self.__buttons_frame = frames.Buttons(self)
 
-        self.__title_frame.pack(**self.PACK_KWARGS)
-        self.__search_frame.pack(**self.PACK_KWARGS)
-        self.__show_frame.pack(**self.PACK_KWARGS, expand=True)
-        self.__filters_frame.pack(**self.PACK_KWARGS)
-        self.__buttons_frame.pack(**self.PACK_KWARGS)
+    #    self.__title_frame.pack(**self.PACK_KWARGS)
+    #    self.__search_frame.pack(**self.PACK_KWARGS)
+    #    self.__show_frame.pack(**self.PACK_KWARGS, expand=True)
+    #    self.__filters_frame.pack(**self.PACK_KWARGS)
+    #    self.__buttons_frame.pack(**self.PACK_KWARGS)
 
-        Thread(target=self.__ask_license, name='Init').start()
+    #    Thread(target=self.__ask_license, name='Init').start()
 
-        self.mainloop()
+    #    self.mainloop()
 
     def __ask_license(self):
         """
