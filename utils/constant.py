c876020416a5b612c9df0ccce2d39cdadf20519e
choulily <106066007+choulily@users.noreply.github.com>
2022-06-20 12:35:19 +0800

    Update constant.py

--- utils/constant.py
+++ utils/constant.py
@@ -21,7 +21,7 @@ class PATH:
     BASE_DIR = BASE_DIR
     WORKING_DIR = os.getcwd()
 
-    if(platform.system() == 'Windows'):
+    if platform.system() == 'Windows'：
         ICON_PATH = os.path.join(BASE_DIR, 'icon.ico')
     else:
         ICON_PATH = None
