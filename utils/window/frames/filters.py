b2ed963a088d278498aa6b98e617100de21d11f5
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-27 13:55:52 +0800

    fix(ui): 🐛 give more space for modpack version in filters

--- utils/window/frames/filters.py
+++ utils/window/frames/filters.py
@@ -15,9 +15,9 @@ class Filters(Frame):
         self.main_window = master
 
         self.sort_label = Label(self, text='排序方式：')
-        self.sort_combobox = Combobox(self, state='readonly')
+        self.sort_combobox = Combobox(self, width=15, state='readonly')
         self.game_version_label = Label(self, text='游戏版本：')
-        self.game_version_combobox = Combobox(self, state='readonly')
+        self.game_version_combobox = Combobox(self, width=10, state='readonly')
         self.modpack_version_label = Label(self, text='整合包版本：')
         self.modpack_version_combobox = Combobox(self, state='readonly')
 
