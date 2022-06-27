b7cb8711309e768cc1e861d52dc116d37ea8ea90
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-27 13:41:41 +0800

    fix(ui): 🐛 fix modpack version filter sorting

--- utils/window/frames/filters.py
+++ utils/window/frames/filters.py
@@ -57,7 +57,7 @@ class Filters(Frame):
         self.game_version_combobox['values'] = SEARCH.VERSIONS
 
     def set_modpack_version(self, values: List[str]):
-        self.modpack_version_combobox['values'] = sorted(values, reverse=True)
+        self.modpack_version_combobox['values'] = values
         self.modpack_version_combobox.current(0)
 
     def on_select(self, event=None):
