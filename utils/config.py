23bb19e44052507554dace3f25feb39f5d90e35d
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-07-02 16:30:24 +0800

    feat(config): ✨ add config file check

--- utils/config.py
+++ utils/config.py
@@ -13,6 +13,14 @@ class Config(dict):
             super().__init__(CONFIG.DEFAULT)
             self.save()
 
+        save_flag = False
+        for key, value in CONFIG.DEFAULT.items():
+            if key not in self.keys():
+                self[key] = value
+                save_flag = True
+        if save_flag:
+            self.save()
+
     def save(self):
         """Save data"""
         with open(CONFIG.FILE_PATH, 'w', encoding='utf-8') as f:
