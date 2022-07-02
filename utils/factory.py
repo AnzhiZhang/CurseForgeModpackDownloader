b63166220a3de48b51b833b7d7eed1534577a77c
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-07-02 14:25:52 +0800

    feat: ✨ add logger to factory

--- utils/factory.py
+++ utils/factory.py
@@ -1,11 +1,18 @@
+from utils.logger import Logger
 from utils.config import Config
 from utils.requester import Requester
+from utils.constant import NAME, PATH
 
 
 class Factory:
     def __init__(self):
+        self.__logger = Logger(NAME, PATH.LOG_FILE_PATH)
         self.__config = Config()
         self.__requester = Requester(self)
+
+    @property
+    def logger(self):
+        return self.__logger
 
     @property
     def config(self):
